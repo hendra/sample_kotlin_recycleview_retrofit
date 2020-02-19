58814fbc0bd7963b7d12ae384e0eff6a4533ffc0
Hendra Nicholas <hendra@41studio.com>
2020-02-19 22:52:01 +0700

    first commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.product_list.*
+
+class MainActivity : AppCompatActivity() {
+    private var products : ArrayList<Product> = ArrayList()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        rvProduct.layoutManager = GridLayoutManager(this, 2)
+
+        var price = 12000
+        for (i in 1..10) {
+            price += i * 10
+            val product = Product("Nama Produk $i", price)
+            products.add(product)
+        }
+
+        rvProduct.adapter = ProductAdapter(products)
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/ProductAdapter.kt
@@ -0,0 +1,42 @@
+package com.example.myapplication
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.product_list.view.*
+
+class ProductAdapter(val products: ArrayList<Product>) : RecyclerView.Adapter<ProductAdapter.ViewHolder>() {
+    override fun getItemCount(): Int = products.size
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        var view : View = LayoutInflater.from(parent.context).inflate(R.layout.product_list, parent, false)
+
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bindData(products.get(position))
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
+        private var view : View = itemView
+        private lateinit var product : Product
+
+        init {
+            itemView.setOnClickListener(this)
+        }
+
+        override fun onClick(p0: View) {
+            Toast.makeText(view.context, "${product.name} Diklik", Toast.LENGTH_SHORT).show()
+        }
+
+        fun bindData(product: Product) {
+            this.product = product
+            view.productImage.setImageResource(R.drawable.product)
+            view.productName.setText(product.name)
+            view.productPrice.setText(product.price.toString())
+        }
+    }
+}
